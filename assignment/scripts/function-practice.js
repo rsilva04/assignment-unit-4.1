26956f5b63dbd15781a5cd1bcbc462cd302d9ad1
Rafael Silva <rafsilva004@gmail.com>
2023-06-26 14:45:44 -0500

    created function for #5

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -42,10 +42,10 @@ function multiplyThree(firstNumber, secondNumber, thirdNumber) {
 //    or greater than zero, and false otherwise
 function isPositive(number) {
   if (number > 0) {
-    return;
+    return true;
   }
-  return;
-}
+  return false;
+}f
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
 console.log('isPositive - should say true', isPositive(3));
